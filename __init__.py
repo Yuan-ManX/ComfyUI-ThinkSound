02e4e5d925606c3b07eae63b99921426c2831fcf
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-07-12 11:48:32 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
+from .nodes import LoadOThinkSoundVideo, LoadCaption, LoadCoTDescription, ThinkSound
 
+NODE_CLASS_MAPPINGS = {
+    "LoadOThinkSoundVideo": LoadOThinkSoundVideo,
+    "LoadCaption": LoadCaption,
+    "LoadCoTDescription": LoadCoTDescription,
+    "ThinkSound": ThinkSound,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadOThinkSoundVideo": "LoadO ThinkSound Video",
+    "LoadCaption": "Load Caption",
+    "LoadCoTDescription": "Load CoT Description",
+    "ThinkSound": "ThinkSound",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
